c8a760fe2903fdddb3bb6417c5f83d11002875e9
excalibur995 <albazzyr@gmail>
2023-03-20 20:35:27 +0700

    adjust - turn off auto reconnect window focus

--- src/drivers/QueryConfig.tsx
+++ src/drivers/QueryConfig.tsx
@@ -5,7 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      refetchOnWindowFocus: process.env.NODE_ENV === "production",
+      refetchOnWindowFocus: false,
     },
   },
 });
